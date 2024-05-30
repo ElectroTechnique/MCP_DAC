fe2c624a12af52a4898c38b567ede824f96e9ec9
Electrotechnique <50934180+ElectroTechnique@users.noreply.github.com>
2024-05-30 18:48:37 +0900

    MCP48FVB21 Mod

--- MCP_DAC.cpp
+++ MCP_DAC.cpp
@@ -6,47 +6,42 @@
 // PURPOSE: Arduino library for MCP_DAC
 //     URL: https://github.com/RobTillaart/MCP_DAC
 
-
 #include "MCP_DAC.h"
-
 
 //  HW SPI
 MCP_DAC::MCP_DAC(__SPI_CLASS__ *inSPI)
 {
-  mySPI     = inSPI;
-  _dataOut  = 255;
-  _clock    = 255;
-  _select   = 0;
-  _hwSPI    = true;
+  mySPI = inSPI;
+  _dataOut = 255;
+  _clock = 255;
+  _select = 0;
+  _hwSPI = true;
   _channels = 1;
   _maxValue = 255;
   reset();
 }
-
 
 //  SW SPI
-MCP_DAC::MCP_DAC(uint8_t dataOut,  uint8_t clock)
+MCP_DAC::MCP_DAC(uint8_t dataOut, uint8_t clock)
 {
-  mySPI     = NULL;
-  _dataOut  = dataOut;
-  _clock    = clock;
-  _select   = 0;
-  _hwSPI    = false;
+  mySPI = NULL;
+  _dataOut = dataOut;
+  _clock = clock;
+  _select = 0;
+  _hwSPI = false;
   _channels = 1;
   _maxValue = 255;
   reset();
 }
-
 
 void MCP_DAC::reset()
 {
-  _gain     = 1;
+  _gain = 1;
   _value[0] = 0;
   _value[1] = 0;
   _buffered = false;
-  _active   = true;
+  _active = true;
 }
-
 
 void MCP_DAC::begin(uint8_t select)
 {
@@ -61,112 +56,128 @@ void MCP_DAC::begin(uint8_t select)
     //  mySPI->end();
     //  mySPI->begin();
   }
-  else  //  software SPI
+  else //  software SPI
   {
     pinMode(_dataOut, OUTPUT);
-    pinMode(_clock,   OUTPUT);
+    pinMode(_clock, OUTPUT);
     digitalWrite(_dataOut, LOW);
-    digitalWrite(_clock,   LOW);
+    digitalWrite(_clock, LOW);
   }
 }
-
 
 uint8_t MCP_DAC::channels()
 {
   return _channels;
 }
 
-
 uint16_t MCP_DAC::maxValue()
 {
   return _maxValue;
 }
 
-
 bool MCP_DAC::setGain(uint8_t gain)
 {
-  if ((0 == gain) || (gain > 2)) return false;
+  if ((0 == gain) || (gain > 2))
+    return false;
   _gain = gain;
   return true;
 }
-
 
 uint8_t MCP_DAC::getGain()
 {
   return _gain;
 }
 
+// bool MCP_DAC::write(uint16_t value, uint8_t channel)
+// {
+//   if (channel >= _channels)
+//     return false;
 
+//   //  CONSTRAIN VALUE
+//   uint16_t _val = value;
+//   if (_val > _maxValue)
+//     _val = _maxValue;
+//   _value[channel] = value;
+
+//   //  PREPARING THE DATA TRANSFER
+//   uint16_t data = 0x1000;
+//   if (channel == 1) // Channel 0 or 1    1001 0000 0000 0000
+//     data |= 0x8000;
+//   if (_buffered) // ?  	0101 0000 0000 0000
+//     data |= 0x4000;
+//   if (_gain == 1) // Gain 0 x2, 1 x1 1100 00 0000 0000
+//     data |= 0x2000;
+
+//   if (_maxValue == 4095)
+//     data |= _val;
+//   else if (_maxValue == 1023)
+//     data |= (_val << 2);
+//   else
+//     data |= (_val << 4);
+//   transfer(data);
+//   return true;
+// }
+
+// ET Write for MCP48FVB21
 bool MCP_DAC::write(uint16_t value, uint8_t channel)
 {
-  if (channel >= _channels) return false;
 
   //  CONSTRAIN VALUE
   uint16_t _val = value;
-  if (_val > _maxValue) _val = _maxValue;
+  if (_val > _maxValue)
+    _val = _maxValue;
   _value[channel] = value;
 
-  //  PREPARING THE DATA TRANSFER
-  uint16_t data = 0x1000;
-  if (channel == 1) data |= 0x8000;
-  if (_buffered)    data |= 0x4000;
-  if (_gain == 1)   data |= 0x2000;
-
-  if (_maxValue == 4095)      data |= _val;
-  else if (_maxValue == 1023) data |= (_val << 2);
-  else                        data |= (_val << 4);
-  transfer(data);
+  transfer(value);
   return true;
 }
-
 
 uint16_t MCP_DAC::lastValue(uint8_t channel)
 {
   return _value[channel];
 }
 
-
 void MCP_DAC::fastWriteA(uint16_t value)
 {
   transfer(0x3000 | value);
 }
-
 
 void MCP_DAC::fastWriteB(uint16_t value)
 {
   transfer(0xB000 | value);
 }
 
-
 bool MCP_DAC::increment(uint8_t channel)
 {
-  if (channel >= _channels) return false;
-  if (_value[channel] == _maxValue) return false;
-  return write(_value[channel] + 1,  channel);
+  if (channel >= _channels)
+    return false;
+  if (_value[channel] == _maxValue)
+    return false;
+  return write(_value[channel] + 1, channel);
 }
-
 
 bool MCP_DAC::decrement(uint8_t channel)
 {
-  if (channel >= _channels) return false;
-  if (_value[channel] == 0) return false;
-  return write(_value[channel] - 1,  channel);
+  if (channel >= _channels)
+    return false;
+  if (_value[channel] == 0)
+    return false;
+  return write(_value[channel] - 1, channel);
 }
-
 
 void MCP_DAC::setPercentage(float perc, uint8_t channel)
 {
-  if (perc < 0) perc = 0;
-  if (perc > 100) perc = 100;
+  if (perc < 0)
+    perc = 0;
+  if (perc > 100)
+    perc = 100;
   write((0.01 * perc * _maxValue), channel);
 }
-
 
 float MCP_DAC::getPercentage(uint8_t channel)
 {
   return (_value[channel] * 100.0) / _maxValue;
 }
-
 
 void MCP_DAC::setLatchPin(uint8_t latchPin)
 {
@@ -174,7 +185,6 @@ void MCP_DAC::setLatchPin(uint8_t latchPin)
   pinMode(_latchPin, OUTPUT);
   digitalWrite(_latchPin, HIGH);
 }
-
 
 void MCP_DAC::triggerLatch()
 {
@@ -188,19 +198,16 @@ void MCP_DAC::triggerLatch()
   }
 }
 
-
 void MCP_DAC::shutDown()
 {
   _active = false;
-  transfer(0x0000);  //  a write will reset the values.
+  transfer(0x0000); //  a write will reset the values.
 }
-
 
 bool MCP_DAC::isActive()
 {
   return _active;
 }
-
 
 void MCP_DAC::setSPIspeed(uint32_t speed)
 {
@@ -208,30 +215,25 @@ void MCP_DAC::setSPIspeed(uint32_t speed)
   _spi_settings = SPISettings(_SPIspeed, MSBFIRST, SPI_MODE0);
 };
 
-
 uint32_t MCP_DAC::getSPIspeed()
 {
   return _SPIspeed;
 }
-
 
 void MCP_DAC::setBufferedMode(bool mode)
 {
   _buffered = mode;
 }
 
-
 bool MCP_DAC::getBufferedMode()
 {
   return _buffered;
 }
 
-
 bool MCP_DAC::usesHWSPI()
 {
   return _hwSPI;
 }
-
 
 //////////////////////////////////////////////////////////////////
 //
@@ -244,18 +246,19 @@ void MCP_DAC::transfer(uint16_t data)
   if (_hwSPI)
   {
     mySPI->beginTransaction(_spi_settings);
+    mySPI->transfer((uint8_t)(data >> 16)); // ET third byte for MCP48FVB21
     mySPI->transfer((uint8_t)(data >> 8));
     mySPI->transfer((uint8_t)(data & 0xFF));
     mySPI->endTransaction();
   }
-  else      //  Software SPI
+  else //  Software SPI
   {
+    mySPI->transfer((uint8_t)(data >> 16)); // ET third byte for MCP48FVB21
     swSPI_transfer((uint8_t)(data >> 8));
     swSPI_transfer((uint8_t)(data & 0xFF));
   }
   digitalWrite(_select, HIGH);
 }
-
 
 //  MSBFIRST
 uint8_t MCP_DAC::swSPI_transfer(uint8_t val)
@@ -270,7 +273,6 @@ uint8_t MCP_DAC::swSPI_transfer(uint8_t val)
   }
   return 0;
 }
-
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -354,7 +356,6 @@ MCP4822::MCP4822(uint8_t dataOut, uint8_t clock) : MCP_DAC(dataOut, clock)
   _maxValue = 4095;
 };
 
-
 /////////////////////////////////////////////////////////////////////////////
 //
 //  MCP4900 series
@@ -417,7 +418,6 @@ MCP4921::MCP4921(uint8_t dataOut, uint8_t clock) : MCP_DAC(dataOut, clock)
 {
   _channels = 1;
   _maxValue = 4095;
-
 };
 
 MCP4922::MCP4922(__SPI_CLASS__ *inSPI) : MCP_DAC(inSPI)
@@ -432,6 +432,4 @@ MCP4922::MCP4922(uint8_t dataOut, uint8_t clock) : MCP_DAC(dataOut, clock)
   _maxValue = 4095;
 };
 
-
 //  -- END OF FILE --
-
--- MCP_DAC.h
+++ MCP_DAC.h
@@ -7,21 +7,18 @@
 // PURPOSE: Arduino library for MCP_DAC
 //     URL: https://github.com/RobTillaart/MCP_DAC
 
-
 #include "Arduino.h"
 #include "SPI.h"
 
-
-#define MCP_DAC_LIB_VERSION       (F("0.5.0"))
+#define MCP_DAC_LIB_VERSION (F("0.5.0"))
 
 #ifndef __SPI_CLASS__
-  #if defined(ARDUINO_ARCH_RP2040)
-  #define __SPI_CLASS__   SPIClassRP2040
-  #else
-  #define __SPI_CLASS__   SPIClass
-  #endif
+#if defined(ARDUINO_ARCH_RP2040)
+#define __SPI_CLASS__ SPIClassRP2040
+#else
+#define __SPI_CLASS__ SPIClass
 #endif
-
+#endif
 
 ///////////////////////////////////////////////////////////////
 //
@@ -36,85 +33,82 @@ public:
   MCP_DAC(uint8_t dataOut, uint8_t clock);
 
   //       both HW and SW SPI.
-  void     begin(uint8_t select);
+  void begin(uint8_t select);
 
   //       returns 0 or 1
-  uint8_t  channels();
+  uint8_t channels();
   //       returns 255 (8 bit), 1023 (10 bit) or 4095 (12 bit)
   uint16_t maxValue();
 
   //       gain = 1 or 2
-  bool     setGain(uint8_t gain = 1);
-  uint8_t  getGain();
+  bool setGain(uint8_t gain = 1);
+  uint8_t getGain();
 
   //       analogWrite became write: see readme.md
-  bool     write(uint16_t value, uint8_t channel = 0);
+  bool write(uint16_t value, uint8_t channel = 0);
   uint16_t lastValue(uint8_t channel = 0);
-  void     fastWriteA(uint16_t value);
-  void     fastWriteB(uint16_t value);
+  void fastWriteA(uint16_t value);
+  void fastWriteB(uint16_t value);
 
-  bool     increment(uint8_t channel = 0);
-  bool     decrement(uint8_t channel = 0);
+  bool increment(uint8_t channel = 0);
+  bool decrement(uint8_t channel = 0);
 
   //       convenience wrappers
   //       percentage = 0..100.0%
-  void     setPercentage(float percentage, uint8_t channel = 0);
-  float    getPercentage(uint8_t channel = 0);
+  void setPercentage(float percentage, uint8_t channel = 0);
+  float getPercentage(uint8_t channel = 0);
 
   //       trigger LDAC = LatchDAC pin - if not set it does nothing
-  void     setLatchPin( uint8_t latchPin);
-  void     triggerLatch();
+  void setLatchPin(uint8_t latchPin);
+  void triggerLatch();
 
   //       shutDown - Page 21  ==> write will wake up.
-  void     shutDown();
-  bool     isActive();
+  void shutDown();
+  bool isActive();
 
   //       speed in Hz
-  void     setSPIspeed(uint32_t speed);
+  void setSPIspeed(uint32_t speed);
   uint32_t getSPIspeed();
 
   //
   //       MCP49xxx series only
   //
   //       see page 20 ==> not functional for MCP48xx series.
-  void     setBufferedMode(bool mode = false);
-  bool     getBufferedMode();
+  void setBufferedMode(bool mode = false);
+  bool getBufferedMode();
 
   //       debugging
-  void     reset();
-  bool     usesHWSPI();
+  void reset();
+  bool usesHWSPI();
   //  uint32_t count();  //  number of writes.
 
 protected:
-  uint8_t  _dataOut;              //  Data out Pin (MOSI)
-  uint8_t  _clock;                //  Clock Pin (SCK)
-  uint8_t  _select;               //  Chip Select Pin (CS)
-  uint8_t  _latchPin = 255;       //  Latch-DAC Pin (LDAC)
-  bool     _hwSPI;                //  Hardware SPI (true) or Software SPI (false)
-  uint32_t _SPIspeed = 16000000;  //  SPI-Bus Frequency
+  uint8_t _dataOut;              //  Data out Pin (MOSI)
+  uint8_t _clock;                //  Clock Pin (SCK)
+  uint8_t _select;               //  Chip Select Pin (CS)
+  uint8_t _latchPin = 255;       //  Latch-DAC Pin (LDAC)
+  bool _hwSPI;                   //  Hardware SPI (true) or Software SPI (false)
+  uint32_t _SPIspeed = 16000000; //  SPI-Bus Frequency
 
-  uint8_t  _channels;             //  Number of DAC-Channels of a given Chip
-  uint16_t _maxValue;             //  Maximum value of a given Chip
-  uint16_t _value[2];             //  Current value  (cache for performance)
-  uint8_t  _gain;                 //  Programmable Gain Amplifier variable
-  bool     _buffered = false;     //  Buffer for the Reference Voltage of the MCP49XX Series Chips
-  bool     _active   = true;      //  Indicates shutDown mode.
+  uint8_t _channels;      //  Number of DAC-Channels of a given Chip
+  uint16_t _maxValue;     //  Maximum value of a given Chip
+  uint16_t _value[2];     //  Current value  (cache for performance)
+  uint8_t _gain;          //  Programmable Gain Amplifier variable
+  bool _buffered = false; //  Buffer for the Reference Voltage of the MCP49XX Series Chips
+  bool _active = true;    //  Indicates shutDown mode.
 
-  void     transfer(uint16_t data);
-  uint8_t  swSPI_transfer(uint8_t d);
+  void transfer(uint16_t data);
+  uint8_t swSPI_transfer(uint8_t d);
 
-  __SPI_CLASS__ * mySPI;
+  __SPI_CLASS__ *mySPI;
 
   SPISettings _spi_settings;
 };
-
-
 
 ///////////////////////////////////////////////////////////////
 //
 //  DERIVED CLASSES
 //
-
 
 ///////////////////////////////////////////////////////////////
 //
@@ -210,6 +204,4 @@ public:
   MCP4922(uint8_t dataOut, uint8_t clock);
 };
 
-
 //  -- END OF FILE --
-
